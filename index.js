71d2f1a337fbb8b44fe7cc9c94a4b8f66917043b
annadevagiri <annadevagiri@gmail.com>
2024-10-29 00:12:35 -0700

    API collection

--- index.js
+++ index.js
@@ -11,18 +11,23 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.use("/", cupcakeStoreRoute);
+app.use('/', cupcakeStoreRoute);
 
-mongoose.connect("mongodb+srv://admin:admin123@cupcake.aj8it.mongodb.net/?retryWrites=true&w=majority&appName=cupcake")
-    .then(() => {
-        console.log("MongoDB connected");
-        app.listen(3000, () => {
-            console.log('Server started on port 3000');
-        });
-    })
-    .catch((error) => {
-        console.log("databasse connection error:::", error);
-    })
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
+// commenting this as I had some connectivity issue and key generation module conflict
+// mongoose.connect("mongodb+srv://admin:admin123@cupcake.aj8it.mongodb.net/?retryWrites=true&w=majority&appName=cupcake")
+//     .then(() => {
+//         console.log("MongoDB connected");
+//         app.listen(3000, () => {
+//             console.log('Server started on port 3000');
+//         });
+//     })
+//     .catch((error) => {
+//         console.log("databasse connection error:::", error);
+//     })
 
 
 
